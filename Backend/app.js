15a3455b18d038d2a9afc7205576aabaa27db60b
Pierre Mattar <pierre00mattar@gmail.com>
2022-08-12 18:41:44 +0300

    changes

---
+++ Backend/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router);
+ // localhost:5000/books
+
+mongoose
+  .connect(
+   "mongodb+srv://Admin:Admin1203@cluster0.1fymh.mongodb.net/BookStore?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
